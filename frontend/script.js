7dc91a09dd5dd7ccba2ed81024289ad20f745a8a
Gabriel_Peixinho <162621920+GabrielPeixinho11ATI@users.noreply.github.com>
2026-09-02 11:27:40 -0300

    atv 3

--- frontend/script.js
+++ frontend/script.js
@@ -1,7 +1,7 @@
 async function buscarFilmes() {
-    // acessar a rota GET do backend, trazer os filmes e mostrar os filmes na tela
-    const resposta = await fetch("https://filmesbackend-eight.vercel.app/") // resposta do backend
-    const filmes = await resposta.json() // converte a resposta num objeto JS
+   
+    const resposta = await fetch("https://filmesbackend-eight.vercel.app/") 
+    const filmes = await resposta.json()
     const sectionFilmes = document.querySelector(".filmes")
 
     filmes.forEach((filme) => {
